87f5e61a9492cd0fe89366a8f169851b3dc6770b
pengwei <pengwei1024@gmail.com>
2017-11-14 22:27:32 +0800

    Log

---
+++ src/com/apkfuns/androidsourceviewer/util/Log.java
@@ -0,0 +1,15 @@
+package com.apkfuns.androidsourceviewer.util;
+
+/**
+ * Created by pengwei on 17/2/23.
+ */
+public class Log {
+
+    private static final boolean DEBUG = true;
+
+    public static void debug(String msg) {
+        if (DEBUG) {
+            System.out.println(msg);
+        }
+    }
+}
