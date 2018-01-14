f81b2d4e908e498c5ce38f56a9f6cd7884dcd003
pengwei08 <pengwei08@baidu.com>
2018-01-14 11:34:57 +0800

    支持JList滚动

--- src/com/apkfuns/androidsourceviewer/widget/GlobalSearchDialog.java
+++ src/com/apkfuns/androidsourceviewer/widget/GlobalSearchDialog.java
@@ -4,38 +4,32 @@ import com.apkfuns.androidsourceviewer.download.SearchDownload;
 import com.apkfuns.androidsourceviewer.entity.ClassEntity;
 import com.apkfuns.androidsourceviewer.util.TextUtils;
 import com.apkfuns.androidsourceviewer.util.ThreadPoolManager;
-import com.intellij.openapi.application.ApplicationManager;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledFuture;
 
 public class GlobalSearchDialog extends JDialog implements ListSelectionListener {
 
     private static final String SEARCH_RESULT_EMPTY = "Result Empty!";
-    private static final String[] SEARCH_EXT = {".java", ".c", ".cpp", "*"};
+    private static final String[] SEARCH_EXT = {".java", ".c", ".cpp", ".cc", "*"};
 
     private JPanel contentPane;
     private JTextField searchBar;
     private JList<String> listView;
     private ScheduledFuture searchTask;
     private final DefaultListModel<String> dataSet = new DefaultListModel<String>();
-    private boolean hasResult = false;
-    private CountDownLatch searchItemCountDown;
 
     public GlobalSearchDialog() {
         setContentPane(contentPane);
         setModal(true);
         setTitle("Android Source Search");
-        setSize(600, 400);
+        setSize(800, 500);
         this.setLocationRelativeTo(null);
-        dataSet.addElement("/frameworks/base/services/java/com/android/server/SystemServer.java");
         listView.setModel(dataSet);
         listView.setFixedCellHeight(25);
         searchBar.addKeyListener(new KeyAdapter() {
